9ad6127721173a7a2a6da6aacd34a1af82bebb6f
Harish Varma Siravuri <harish.siravuri@gmail.com>
2023-10-19 12:29:18 -0500

    restructured into a package (#6)

---
+++ gnnnas/__init__.py
@@ -0,0 +1,5 @@
+from gnnnas.data import DataLoader
+from gnnnas.models import MPNN
+from gnnnas.util import Util
+
+__all__ = ["DataLoader", "MPNN", "Util"]
---
+++ tests/test_data.py
@@ -0,0 +1,6 @@
+from gnnnas import DataLoader
+
+
+def test_answer_question() -> None:
+    sample_obj = DataLoader()
+    assert True
